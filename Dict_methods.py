4e110a7418fd317ef8af06cf023c9f72c5d23917
Gagan jha <gkjha1495@gmail.com>
2025-06-25 22:43:20 +0530

    Add files via upload

---
+++ Dict_methods.py
@@ -0,0 +1,17 @@
+d = {} # empty dictionary
+
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# marks.update({"Harry": 99})
+# print(marks)
+
+print(marks.get("Shivika")) # prints none
+print(marks["Harry2"]) # return error

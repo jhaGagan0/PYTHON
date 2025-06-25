11dcc1f4ff8b5a5d4fceabcc119bcba2598c3e81
Gagan jha <gkjha1495@gmail.com>
2025-06-25 22:44:10 +0530

    Add files via upload

---
+++ sets_methods.py
@@ -0,0 +1,11 @@
+s = {1, 5,32,54,5,5,5,5,5,5,5}
+t = {4,3,8,6,44,5,7}
+# print(s, type(s))
+
+# s.add(566)
+# print(s, type(s))
+# s.remove(1)
+# print(s, type(s))
+
+print(s.union(t))
+print(s.intersection(t))

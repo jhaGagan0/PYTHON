a78e04747512ad3e561e775ce1e6dd2dbdc7a7de
Gagan jha <gkjha1495@gmail.com>
2025-07-03 23:32:27 +0530

    Add files via upload

---
+++ q6_emptydictionary.py
@@ -0,0 +1,23 @@
+s = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+s.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+s.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+s.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+s.update({name: lang})
+
+print(s)

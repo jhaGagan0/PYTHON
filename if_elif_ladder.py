0404adec9d181e1e9da3a5381d9c466dbcb6fcb7
Gagan jha <gkjha1495@gmail.com>
2025-07-06 23:37:40 +0530

    Add files via upload

---
+++ if_elif_ladder.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age : "))
+
+if(a>=18):
+    print("You are adult")
+
+elif(a<=0):
+    print("Enter a valid age (not negative nor zero)")
+
+else:
+    print("You are not adult")

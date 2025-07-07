f312dc79427081a7a751a1082b0dd10aa5f4db2b
Gagan jha <gkjha1495@gmail.com>
2025-07-07 23:38:23 +0530

    Add files via upload

---
+++ Q1_Greatestnoo.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a no. : "))
+b = int(input("Enter a no. : "))
+c = int(input("Enter a no. : "))
+d = int(input("Enter a no. : "))
+
+if(a>b and a>c and a>d):
+    print(a)
+if(b>a and b>c and b>d):
+    print(b)
+if(c>b and c>a and c>d):
+    print(c)
+if(d>b and d>c and d>a):
+    print(d)

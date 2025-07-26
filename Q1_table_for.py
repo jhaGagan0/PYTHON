5cf0ce4268f55e087e9a53e91c5a146fb32ec3b2
Gagan jha <gkjha1495@gmail.com>
2025-07-26 23:40:07 +0530

    Add files via upload

---
+++ Q1_table_for.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number : "))
+
+for i in range (a,(a*11),a):
+    print(i)
---
+++ for_loops.py
@@ -0,0 +1,20 @@
+for i in range(0, 100, 4):
+    print(i)
+
+
+
+
+# l = [1, 4, 6, 234, 6, 764]
+
+# for i in l:
+#     print(i)
+    
+# t = (6, 231, 75, 122)
+# for i in t:
+#     print(i)
+    
+# s = "Gagan"
+# for i in s:
+#     print(i)
+
+
